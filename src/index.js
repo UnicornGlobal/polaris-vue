ea6d682e1df26555c239726d98bc10336db385f8
Darryn Ten <darryn.ten@gmail.com>
2020-05-28 16:55:13 +0700

    Autoloading

--- src/index.js
+++ src/index.js
@@ -5,12 +5,21 @@
 export default {
   install(Vue) {
     const components = require.context('./components', true, /\.vue$/i)
-    console.log('installing')
-    for (const component in components) {
-      console.log('component', component)
-      const name = component.replace(/([a-z0-9]|(?=[A-Z]))([A-Z])/g, '$1-$2').toLowerCase()
-      console.log(name, '<')
-      Vue.component(name, components[component])
-    }
+
+    console.log('installing', components)
+
+    components.keys().map((key) => {
+      console.log('key', key)
+      const n = key.split('/').pop().split('.')[0]
+      console.log('n>', n)
+      Vue.component(n, components(key).default)
+    })
+
+    // for (const component in components) {
+      // console.log('component', component)
+      // const name = component.replace(/([a-z0-9]|(?=[A-Z]))([A-Z])/g, '$1-$2').toLowerCase()
+      // console.log(name, '<')
+      // Vue.component(name, components[component])
+    // }
   }
 }
