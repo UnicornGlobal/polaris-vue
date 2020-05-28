27adc0fc0a98272a906d9645014dd3909bca3cba
Darryn Ten <darryn.ten@gmail.com>
2020-05-28 16:38:28 +0700

    Installer

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ export default {
   install(Vue) {
     for (const component in components) {
       const name = component.replace(/([a-z0-9]|(?=[A-Z]))([A-Z])/g, '$1-$2').toLowerCase()
-      console.log(name)
+      console.log(name, '<')
       Vue.component(name, components[component])
     }
   }
