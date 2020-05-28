830d46675ef8fe1c5ff41a33a1e0944391d4cb01
Darryn Ten <darryn.ten@gmail.com>
2020-05-28 16:36:27 +0700

    Build

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const components = require.context('./components', true, /\.vue$/)
 
 export default {
-    install(Vue) {
-        for (const component in components) {
-            Vue.component(component.replace(/([a-z0-9]|(?=[A-Z]))([A-Z])/g, '$1-$2').toLowerCase(), components[component])
-        }
+  install(Vue) {
+    for (const component in components) {
+      const name = component.replace(/([a-z0-9]|(?=[A-Z]))([A-Z])/g, '$1-$2').toLowerCase()
+      console.log(name)
+      Vue.component(name, components[component])
     }
+  }
 }
