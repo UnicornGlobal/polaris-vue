902cbc7e34abc5d4b1b996bb9199ec45452611da
Darryn Ten <darryn.ten@gmail.com>
2020-05-28 16:11:55 +0700

    Rebuild

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-// const components = require.context('./components', true, /\.vue$/)
+const components = require.context('./components', true, /\.vue$/)
 
 export default {
     install(Vue, options) {
         options = Object.assign(defaults, options)
 
-        // for (const component in components) {
-            // Vue.component(component.replace(/([a-z0-9]|(?=[A-Z]))([A-Z])/g, '$1-$2').toLowerCase(), components[component])
-        // }
+        for (const component in components) {
+            Vue.component(component.replace(/([a-z0-9]|(?=[A-Z]))([A-Z])/g, '$1-$2').toLowerCase(), components[component])
+        }
     }
 }
