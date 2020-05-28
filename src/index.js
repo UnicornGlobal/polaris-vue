506097808db496405521312b6f5337c1b9899f31
Darryn Ten <darryn.ten@gmail.com>
2020-05-28 16:33:03 +0700

    Bump version

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 const components = require.context('./components', true, /\.vue$/)
 
 export default {
-    install(Vue, options) {
-        options = Object.assign(defaults, options)
-
+    install(Vue) {
         for (const component in components) {
             Vue.component(component.replace(/([a-z0-9]|(?=[A-Z]))([A-Z])/g, '$1-$2').toLowerCase(), components[component])
         }
